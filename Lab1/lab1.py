e22a014286ed8580cc7ff5cd362f7c93c65a1f02
fcarli3 <francescocarli79@gmail.com>
2021-10-01 14:13:17 +0200

    Tests

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -8,4 +8,24 @@ def sum(m,n):
     else:
         for i in range(n):
             result += 1
+
     return result
+
+
+def divide(m,n):
+    res = 0;
+    negativeRes = m > 0 and n < 0 or m < 0 and n > 0;
+
+    n = abs(n);
+    m = abs(m);
+
+    if(n == 0):
+        raise ZeroDivisionError;
+
+    while (m - n >= 0):
+        m -= n;
+        res += 1;
+
+    res = -res if negativeRes else res;
+
+    return res;
