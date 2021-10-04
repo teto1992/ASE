de2ad4dd69709539a2cf0c9474bd4710b75e1841
TommasoLencioni <lencio97@gmail.com>
2021-10-04 23:03:14 +0200

    add division and pass unit tests

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -9,3 +9,18 @@ def sum(m,n):
         for i in range(n):
             result += 1
     return result
+
+def divide(m,n):
+    if n==0:
+        raise ZeroDivisionError
+    negative=(((m<0) and  not (n<0)) or (not (m<0) and (n<0)))
+    i=-1
+    m=abs(m)
+    n=abs(n)
+    while m>=0:
+        m-=n
+        i+=1
+    if negative:
+        return i*-1
+    else:
+        return i
